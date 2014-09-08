0d91abb89666cc9d3e2259953dba60e703f4fbf9
PewPewPew2 <tschweitzer@live.com>
2014-09-07 20:00:25 -0600

    Update SidasAutoCarryPlugin - Talon.lua

--- SidasAutoCarryPlugin - Talon.lua
+++ SidasAutoCarryPlugin - Talon.lua
@@ -13,34 +13,28 @@ function Plugin:OnTick()
 	Checks()
 	Target = AutoCarry.Crosshair:GetTarget()
 	
-	--if Menu.Harass then ItemTest() end
+	if Menu.Harass then ItemTest() end
 	if AutoCarry.Keys.AutoCarry then Combo() end
 	if AutoCarry.Keys.MixedMode then Harass() end	
 end
 
---[[local itemSlot
+
 local Items = {
-	[3077] = {name = "Tiamat"},
-	[3074] = {name = "Tiamat"},
-	[3142] = {name = "Tiamat"},
-	[3144] = {name = "Tiamat"},
-	[3153] = {name = "Tiamat"},
-}
-
-local HaveItem = {
-
+	[3144] = {name = "BilgewaterCutlass"},
+	[3153] = {name = "ItemSwordOfFeastAndFamine"},
+	[3077] = {name = "ItemTiamatCleave"},
+	[3074] = {name = "ItemTiamatCleave"},
+	[3142] = {name = "YoumusBlade"},
 }
 
 function ItemTest()
 	for i, _ in pairs(Items) do
-		local itemId = i --:gsub("%s+", "")
-		local slot = GetInventorySlotItem(itemID)
-		print(""..slot.."")
-		--if slot then 
-			--HaveItem[slot]
-		--end
+		local slot = GetInventorySlotItem(i)
+		if GetInventoryItemIsCastable(i) then
+			Packet("S_CAST", { spellId = slot, targetNetworkId = Target.networkID }):send()
+		end
 	end
-end]]
+end
 
 function Checks()
 	QAble = (myHero:CanUseSpell(_Q) == READY)
@@ -64,8 +58,15 @@ function Combo()
 		elseif not EAble and WAble then
 			local wPos = Vector(myHero) + Vector(Vector(Target) - Vector(myHero)):normalized()*660
 			Packet("S_CAST", { spellId = _W, toX = wPos.x, toZ = wPos.z }):send()
-		elseif ValidTarget(Target, 275) and AutoCarry.Orbwalker:IsAfterAttack() and not WAble then
-			Packet("S_CAST", { spellId = _Q}):send()	
+		elseif ValidTarget(Target, 300) and AutoCarry.Orbwalker:IsAfterAttack() and not WAble then
+			Packet("S_CAST", { spellId = _Q }):send()	
+		elseif Menu.useItems and ValidTarget(Target, 275) then
+			for i, _ in pairs(Items) do
+				local slot = GetInventorySlotItem(i)
+				if GetInventoryItemIsCastable(i) then
+					Packet("S_CAST", { spellId = slot, targetNetworkId = Target.networkID }):send()
+				end
+			end			
 		else
 			Harass()
 		end	
@@ -76,31 +77,22 @@ function Harass()
 	if Target and ValidTarget(Target, 660) and EAble and QAble and WAble then 
 		Packet("S_CAST", { spellId = _E, targetNetworkId = Target.networkID }):send()
 	elseif ValidTarget(Target, 275) and not WAble and AutoCarry.Orbwalker:IsAfterAttack() then
-		Packet("S_CAST", { spellId = _Q}):send()
+		Packet("S_CAST", { spellId = _Q }):send()
 	end	
 end
 
 function OnGainBuff(unit, buff)
 	if Target and unit.networkID == Target.networkID and buff.name == "talondamageamp" then
 		if AutoCarry.Keys.AutoCarry then
-			DelayAction(function() Packet("S_CAST", { spellId = _R }):send() end, 0.4)
+			DelayAction(function() Packet("S_CAST", { spellId = _R }):send() end, 0.8)
 		end	
 		local wPos = Vector(myHero) + Vector(Vector(Target) - Vector(myHero)):normalized()*300
 		DelayAction(function() Packet("S_CAST", { spellId = _W, toX = wPos.x, toZ = wPos.z }):send() end, 0.5)
 	end
 end
 
---[[function GetInventorySlotItem(itemID, target)
-    assert(type(itemID) == "number", "GetInventorySlotItem: wrong argument types ( expected)")
-    local target = target or player
-    for _, j in pairs({ ITEM_1, ITEM_2, ITEM_3, ITEM_4, ITEM_5, ITEM_6, ITEM_7 }) do
-        if target:getInventorySlot(j) == itemID then return j end
-    end
-    return nil
-end
-
-
-Menu:addParam("Combo", "Combo Hotkey", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("W"))
-Menu:addParam("Harass", "Harass Hotkey", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("X"))]]
 Menu = AutoCarry.Plugins:RegisterPlugin(Plugin(), "Talon Combo")
+Menu:addParam("Harass", "Debug key", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("X"))
+Menu:addParam("useItems", "Use Items in Combo", SCRIPT_PARAM_ONOFF, false)
+
   
